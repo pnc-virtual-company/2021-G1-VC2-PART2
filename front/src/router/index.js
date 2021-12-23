756f2183f342c0c952888b4143aaf64fe7a1995e
chantheaphonIT <chanthea.phon@student.passerellesnumeriques.org>
2021-12-23 10:56:16 +0700

    Create card form

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Permission from '../components/views/Permission.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  
+    { path: "/", component: Permission }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
