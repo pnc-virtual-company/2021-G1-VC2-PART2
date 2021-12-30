d8046457babe7fb40887fe86a702792a493147a1
vunyin <vun.yin@student.passerellesnumeriques.org>
2021-12-30 08:54:11 +0700

    #11 manage role user

--- front/src/components/views/Disciple.vue
+++ front/src/components/views/Disciple.vue
@@ -51,7 +51,7 @@
           </v-row>
         </v-container>
 
-        <v-card-actions>
+        <v-card-actions > 
           <v-spacer></v-spacer>
           <v-btn color="gray darken-1" text @click="editdialog = false">Cancel</v-btn>
           <v-btn color="green darken-1" text @click="Update(id)">OK</v-btn>
@@ -67,7 +67,7 @@
       <v-card class="cardForm">
         <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
         <v-divider></v-divider>
-        <v-card-actions>
+        <v-card-actions >
           <v-spacer></v-spacer>
           <v-btn color="gray darken-1" text @click="dialog = false">Cancel</v-btn>
           <v-btn color="green darken-1" text @click="Remove(id)">OK</v-btn>
@@ -142,7 +142,7 @@
             </div>
           </v-col>
 
-          <v-col cols="12" sm="2">
+          <v-col cols="12" sm="2"  v-if="userRole !== 'Student' && userRole !== 'Social Affair' ">
             <!-- ==============start button edit&delete============= -->
             <v-list-item-icon >
                 <v-icon class="edit" @click ="ShowDilogEdit(disciple)" color='green'>mdi-pencil-box-multiple-outline</v-icon>
