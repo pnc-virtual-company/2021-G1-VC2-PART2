08c832f1a9378a0f8842cd4b5d1cf7d5f1e6c728
sreytouchmouch <sreytouch.mouch@student.passerellesnumeriques.org>
2021-12-30 19:15:49 +0700

    #31 manage search discipline

--- front/src/components/views/Disciple.vue
+++ front/src/components/views/Disciple.vue
@@ -1,168 +1,186 @@
 <template>
   <v-expansion-panels class="main">
     <div class="headerManin">
-    <!-- ===================Edit disciple dialog======================== -->
+      <!-- ===================Edit disciple dialog======================== -->
 
-    <v-dialog v-model="editdialog" max-width="600px">
-      <v-card class="card">
-        <v-card-title color='blue' class="text-h6">
-          <span>Are you sure you want to update this item?</span>
-        </v-card-title>
-        <v-container>
-          <v-row>
-            <v-col
-                cols="6"
-                sm="6"
-            >
+      <v-dialog v-model="editdialog" max-width="600px">
+        <v-card class="card">
+          <v-card-title color="blue" class="text-h6">
+            <span>Are you sure you want to update this item?</span>
+          </v-card-title>
+          <v-container>
+            <v-row>
+              <v-col cols="6" sm="6">
                 <v-autocomplete
-                    v-model="student_id"
-                    label="Choose student"
-                    rows='1'
-                    prepend-icon='mdi-account'
+                  v-model="student_id"
+                  label="Choose student"
+                  rows="1"
+                  prepend-icon="mdi-account"
                 ></v-autocomplete>
-            </v-col>
+              </v-col>
 
-            <v-col
-              cols="6"
-              sm="6"
-            >
+              <v-col cols="6" sm="6">
                 <v-autocomplete
                   v-model="dnt"
                   :items="leavelist"
                   label="Choose discipline notice type"
                   multiple
-                  rows='1'
-                  prepend-icon='mdi-alert-outline'
+                  rows="1"
+                  prepend-icon="mdi-alert-outline"
                 ></v-autocomplete>
-            </v-col>
-        
-            <v-col
-              cols="12"
-              sm="12"
+              </v-col>
+
+              <v-col cols="12" sm="12">
+                <v-textarea
+                  v-model="description"
+                  class="mx-2"
+                  label="Description"
+                  rows="1"
+                  prepend-icon="mdi-comment-processing-outline"
+                ></v-textarea>
+              </v-col>
+            </v-row>
+          </v-container>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="gray darken-1" text @click="editdialog = false"
+              >Cancel</v-btn
             >
-            <v-textarea
-              v-model="description"
-              class="mx-2"
-              label="Description"
-              rows="1"
-              prepend-icon="mdi-comment-processing-outline"
-          ></v-textarea>
-          </v-col>
-          </v-row>
-        </v-container>
+            <v-btn color="green darken-1" text @click="Update(id)">OK</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!-- ==========================End edit Dialog===================================== -->
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="gray darken-1" text @click="editdialog = false">Cancel</v-btn>
-          <v-btn color="green darken-1" text @click="Update(id)">OK</v-btn>
-        
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <!-- ==========================End edit Dialog===================================== -->
+      <!-- ===================Delete disciple dialog======================== -->
 
-    <!-- ===================Delete disciple dialog======================== -->
+      <v-dialog v-model="dialog" max-width="500px">
+        <v-card class="cardForm">
+          <v-card-title class="text-h5"
+            >Are you sure you want to delete this item?</v-card-title
+          >
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="gray darken-1" text @click="dialog = false"
+              >Cancel</v-btn
+            >
+            <v-btn color="green darken-1" text @click="Remove(id)">OK</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!-- ==========================End Dialog===================================== -->
 
-    <v-dialog v-model="dialog" max-width="500px">
-      <v-card class="cardForm">
-        <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="gray darken-1" text @click="dialog = false">Cancel</v-btn>
-          <v-btn color="green darken-1" text @click="Remove(id)">OK</v-btn>
-          
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <!-- ==========================End Dialog===================================== -->
+      <!-- ===================search============= -->
+      <div class="cardheader">
+        <v-card-title>
+          <v-text-field
+            v-if="userRole !== 'Student' && userRole !== 'Social Affair'"
+            class="searchbtn"
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            color="blue darken-1"
+            @keyup="searchBotton"
+          ></v-text-field>
+        </v-card-title>
+        <form-disciple
+          v-if="userRole == 'Admin'"
+          @add-discipline="getDisciples"
+        ></form-disciple>
+      </div>
 
-<!-- ===================search============= -->
-    <div class="cardheader">
-      <v-card-title>
-        <v-text-field
-          v-if="userRole !== 'Student' && userRole !== 'Social Affair' "
-          class="searchbtn"
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search"
-          color="blue darken-1"
-          v-on:keyup="searchBotton"
-        ></v-text-field>
-      </v-card-title>
-      
-      <form-disciple v-if="userRole == 'Admin' " @add-discipline="getDisciples"></form-disciple>
-    </div>
-    
-<!-- ==========card=============== -->
-  
-    <v-expansion-panel class="formCard"
-      v-for="disciple of disciples" :key="disciple.id">
-      <v-expansion-panel-header class="header">
-        <v-row>
-          <v-col cols="12" sm="1">
-            <v-img
-              id="wearning"
-              max-height="70"
-              max-width="70"
-              src="../../assets/wl.png">
-            </v-img>
-            
-          </v-col>
-          <v-col cols="12" sm="2">
-            <p id="dnt">{{disciple.dnt}}</p>
-          </v-col>
-          
-          <v-col cols="12" sm="3">
+      <!-- ==========card=============== -->
+
+      <v-expansion-panel
+        class="formCard"
+        v-for="disciple of disciples"
+        :key="disciple.id"
+      >
+        <v-expansion-panel-header class="header">
+          <v-row>
+            <v-col cols="12" sm="1">
+              <v-img
+                id="wearning"
+                max-height="70"
+                max-width="70"
+                src="../../assets/wl.png"
+              >
+              </v-img>
+            </v-col>
+            <v-col cols="12" sm="2">
+              <p id="dnt">{{ disciple.dnt }}</p>
+            </v-col>
+
+            <v-col cols="12" sm="3">
               <v-img
                 max-height="120"
                 max-width="90"
-                :src="url + disciple.student.picture">
+                :src="url + disciple.student.picture"
+              >
               </v-img>
-          </v-col >
+            </v-col>
 
-          <v-col cols="12" sm="4">
-            <div class="username" >
-              <div>
-                <h3 style="margin-bottom: 10px;">{{ disciple.student.firstName }} {{ disciple.student.lastName }}</h3>
+            <v-col cols="12" sm="4">
+              <div class="username">
+                <div>
+                  <h3 style="margin-bottom: 10px">
+                    {{ disciple.student.firstName }}
+                    {{ disciple.student.lastName }}
+                  </h3>
                 </div>
                 <p>{{ disciple.class }}</p>
-            </div>
-        
-            <div style="disply:flex;" >
-                <div class="date" style="margin-left: 58px; margin-bottom: -23px;" >
-                    <h4 >Jan/23/2021</h4>
+              </div>
+
+              <div style="disply: flex">
+                <div
+                  class="date"
+                  style="margin-left: 58px; margin-bottom: -23px"
+                >
+                  <h4>Jan/23/2021</h4>
                 </div>
 
                 <v-img
                   max-height="50"
                   max-width="50"
-                  src="../../assets/date.png" alt="">
+                  src="../../assets/date.png"
+                  alt=""
+                >
                 </v-img>
-            </div>
-          </v-col>
+              </div>
+            </v-col>
 
-          <v-col cols="12" sm="2">
-            <!-- ==============start button edit&delete============= -->
-            <v-list-item-icon >
-                <v-icon class="edit" @click ="ShowDilogEdit(disciple)" color='green'>mdi-pencil-box-multiple-outline</v-icon>
-            </v-list-item-icon>
+            <v-col cols="12" sm="2">
+              <!-- ==============start button edit&delete============= -->
+              <v-list-item-icon>
+                <v-icon
+                  class="edit"
+                  @click="ShowDilogEdit(disciple)"
+                  color="green"
+                  >mdi-pencil-box-multiple-outline</v-icon
+                >
+              </v-list-item-icon>
 
-            <v-list-item-icon  >
-                <v-icon class="delete" @click="ShowDialog(disciple)" color="#EF5350">mdi-delete</v-icon>
-            </v-list-item-icon>
-          </v-col>
-         
-        </v-row>
-        <!-- ===================end button edit&delete============= -->
-      </v-expansion-panel-header>
-      <!-- ====================start show details========== -->
-      <v-expansion-panel-content>
-        {{ disciple.description }} 
-      </v-expansion-panel-content>
+              <v-list-item-icon>
+                <v-icon
+                  class="delete"
+                  @click="ShowDialog(disciple)"
+                  color="#EF5350"
+                  >mdi-delete</v-icon
+                >
+              </v-list-item-icon>
+            </v-col>
+          </v-row>
+          <!-- ===================end button edit&delete============= -->
+        </v-expansion-panel-header>
+        <!-- ====================start show details========== -->
+        <v-expansion-panel-content>
+          {{ disciple.description }}
+        </v-expansion-panel-content>
 
-       <!-- ====================end show details=================== -->
-    </v-expansion-panel>
+        <!-- ====================end show details=================== -->
+      </v-expansion-panel>
     </div>
   </v-expansion-panels>
 </template>
@@ -217,7 +235,7 @@ export default {
       this.id=disciple.id;
       this.description = disciple.description;
       this.dnt = disciple.type;
-      console.log(disciple);
+      // console.log(disciple);
 
       this.editdialog = true;
     },
@@ -246,6 +264,7 @@ export default {
           }else{
               this.disciples = res.data;
             }
+        console.log("disciples",this.disciples);
       })
     },
     ShowDialog(disciple){
@@ -255,76 +274,73 @@ export default {
     getStudent(){
       axios.get('/students').then(res => {
         this.studentlist = res.data;
-        console.log(res.data);
       });
     },
-     searchBotton(){
-            if(this.search!== ""){
-                axios.get("/disciples/search/" + this.search).then(res => {
-                this.disciples = res.data;
-                })
-            }else{
-                this.getDisciples();
-            }           
-        }        
+    searchBotton(){
+      if(this.search !== "") {
+        this.disciples = this.disciples.filter(
+          (disciple) => (disciple.student.firstName.toLowerCase().includes(this.search.toLowerCase())||
+          (disciple.student.lastName.toLowerCase().includes(this.search.toLowerCase()))))
+      }else{
+        this.getDisciples();
+      }
+    },
+        
   },
-  
   mounted() {
     this.getDisciples();
     this.getStudent();
     this.userRole = localStorage.getItem('role');
   },
-
-  
 }
 </script>
 
 <style scoped>
-  .cardheader{
-    display: flex;
-    align-items: flex-start;
-    justify-content: space-between;
-  }
-  .headerManin{
-    width: 80%;
-  }
-  .username{
-    padding: 10px;
-    margin: 10px; 
-  }
-  .v-icon {
-    margin-left: 170%;
-    padding: 5px;
- }
- .delete {
-    margin-left: 40%;
-    padding: 5px;
-     margin-top: 11px;
- }
- .edit{
-    margin-left: 100%;
-    padding: 5px;
-    margin-top: 11px;
- }
-  .header{
-    margin-right: 5%;
-  }
-  .main{
-    height: 84vh;
-    overflow-y: scroll;
-    margin-top: 3%;
-  }
-  .cardForm{
-    border-top: 5px solid red;
-  }
-  #dnt{
-    margin-top: 25%;
-  }
-  #wearning{
-    margin-top: 25%;
-  }
-  .searchbtn{
-    margin-left: -7%;
-    margin-top: -9%;
-  }
+.cardheader {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+}
+.headerManin {
+  width: 80%;
+}
+.username {
+  padding: 10px;
+  margin: 10px;
+}
+.v-icon {
+  margin-left: 170%;
+  padding: 5px;
+}
+.delete {
+  margin-left: 40%;
+  padding: 5px;
+  margin-top: 11px;
+}
+.edit {
+  margin-left: 100%;
+  padding: 5px;
+  margin-top: 11px;
+}
+.header {
+  margin-right: 5%;
+}
+.main {
+  height: 84vh;
+  overflow-y: scroll;
+  margin-top: 3%;
+}
+.cardForm {
+  border-top: 5px solid red;
+}
+#dnt {
+  margin-top: 25%;
+}
+#wearning {
+  margin-top: 25%;
+}
+.searchbtn {
+  margin-left: -7%;
+  margin-top: -9%;
+}
 </style>
